a1a3aeb0758dbfe0164139e5606bc53591d6cc23
Rafaela Marraschi <rafaela.vm@gmail.com>
2018-02-10 14:11:32 -0200

    Subindo o projeto - 44. App1 - Criando o componente Tentativas e Progresso via CLI

---
+++ app1/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import {TopoComponent} from './topo/topo.component';
+import { PainelComponent } from './painel/painel.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopoComponent,
+    PainelComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ app1/src/app/topo/topo.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'app-topo', //seletor baseado em classe
+    templateUrl: './topo.component.html',
+    styleUrls: ['./topo.component.css']
+    //template: `<p>
+                //Esse é o componente topo
+                  //</p>`
+
+})
+
+export class TopoComponent {
+
+
+}
