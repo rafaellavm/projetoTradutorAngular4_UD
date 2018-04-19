f44563f5ebfae48af7ba79f3b096653735bda597
Rafaela Marraschi <rafaela.vm@gmail.com>
2018-04-19 17:41:56 -0300

    75 - Controlando tentativaas parte 2

--- app1/src/app/tentativas/tentativas.component.ts
+++ app1/src/app/tentativas/tentativas.component.ts
@@ -13,22 +13,31 @@ export class TentativasComponent implements OnInit, OnChanges {
   public coracoes: Coracao[] = [
     new Coracao(true), //como no construtor os outros parãmetros são default não precisa passar
     new Coracao(true),
-    new Coracao(false)
+    new Coracao(true)
   ];
 
-  constructor() { 
-    console.log(this.coracoes);
-    
+  constructor() {
+    //console.log(this.coracoes);
+
   }
 
- //é solicitado quando há input dos dados do pai pro filho. Quando os valores recebidos são alterados o método também é executado
-  ngOnChanges(){
+  //é solicitado quando há input dos dados do pai pro filho. Quando os valores recebidos são alterados o método também é executado
+  ngOnChanges() {
+
+    if (this.tentativas != this.coracoes.length) {
+
+      let indice = this.coracoes.length - this.tentativas;
+      this.coracoes[indice - 1].cheio = false; //coloca o coração falso na tentativa em questão
+    }
+
+    console.log('array de coracoes = ', this.coracoes);
     console.log('tentativas recebidas do painel = ', this.tentativas);
+    console.log('númeo de corações painel = ', this.coracoes.length);
   }
 
   // método do ciclo de vida do componente// É solicitado uma única vez quando o componente é iniciado
-  ngOnInit() { 
-    
+  ngOnInit() {
+
   }
 
 }
